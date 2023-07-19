9565e2918cb53b6e80a03151bde1e430caacf19a
master <enoch.binas@gmail.com>
2023-07-19 16:46:46 +0800

    styles added to cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,14 @@
 import { Fragment } from "react";
 import Nav from "./Nav";
 import { useSelector } from "react-redux";
+import { Box, Button, Divider } from "@chakra-ui/react";
+import { useToast } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
 
 function Cart() {
 
     const cart = useSelector((state) => state.cart.value)
+    const toast = useToast()
 
     console.log(cart);
     return(
@@ -12,12 +16,29 @@ function Cart() {
             <Nav />
             {cart.map((items) => {
                 return (
-                    <Fragment key={items.name}>
-                        <h1>{items.price}</h1>
-                        <h1>{items.name}</h1>
-                        <h1>{items.img}</h1>
-                        <h1>Quantity -- {items.quantity} ${items.totals}</h1>
-                     </Fragment>
+                    <div key={items.name} className='flex gap-5 md:w-1/3 md:mx-auto border md:justify-around p-10'>
+                            <Image src={items.img} alt='computer hardware part' className='w-1/2  md:w-32 md:h-24 md:relative md:top-5'/>
+                        <section className='flex-col'>
+                            <h1 className='text-2xl'>{items.name}</h1>
+                            <div className='flex-col'>
+                                <footer className='flex gap-3 my-5'>
+                                        <p>{items.quantity} - </p>
+                                        <p>${items.totals}</p>
+                                        
+                                </footer>
+                                <Button size={{base: 'sm', md: 'lg'}}
+                                        onClick={() => toast({
+                                            title: 'Order submitted',
+                                            description: 'Deliver is on your way!',
+                                            status: 'success',
+                                            duration: '2000',
+                                            isClosable: true,
+                                        })}>
+                                    Place order
+                                </Button>                               
+                            </div>
+                        </section>
+                    </div>
                 )
             })}
         </>
